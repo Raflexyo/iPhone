ba7b00673144bafd42e71309d927be07ee8b9e70
Raflexyo <123810563+Raflexyo@users.noreply.github.com>
2023-05-31 12:39:59 +0200

    se

---
+++ MessagesUser.jsx
@@ -0,0 +1,207 @@
+import React, { useState, useEffect, useRef } from "react";
+import "../css/Messages.css";
+import Draggable from "react-draggable";
+import { useNavigate, useParams } from "react-router-dom";
+import axios from "axios";
+
+export default function MessagesUser({ response }) {
+  const navigate = useNavigate();
+  const [initialY, setInitialY] = useState(0);
+  const { id } = useParams();
+  const [messages, setMessages] = useState([]);
+  const messageInputRef = useRef(null);
+  const messageContainerRef = useRef(null);
+  const UsrID = response?.id;
+  const ws = useRef(null);
+  const [info, setInfo] = useState(null);
+
+  const handleDragStart = (_, { y }) => {
+    setInitialY(y);
+  };
+
+  const handleDrag = (_, { y }) => {
+    if (y < initialY) {
+      navigate("/");
+    } else {
+      setInitialY(y);
+    }
+  };
+
+  const fetchUserInfo = async () => {
+    try {
+      const response = await axios.get(`http://localhost:8001/user/${id}`);
+      const name = response.data[0].name;
+      const surname = response.data[0].surname;
+      const img = response.data[0].img;
+      setInfo({ name, surname, img });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchUserInfo();
+  }, []);
+
+  const fetchMessages = async () => {
+    try {
+      const response = await axios.get(
+        `http://localhost:8001/messages?sender_id=${UsrID}&recipient_id=${id}`
+      );
+      setMessages(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+
+  const formatTimestamp = (timestamp, index) => {
+    const currentDate = new Date();
+    const messageDate = new Date(timestamp);
+    const timezoneOffset = messageDate.getTimezoneOffset() * 60 * 1000;
+    const adjustedMessageDate = new Date(messageDate.getTime() - timezoneOffset);
+  
+    const optionsToday = {
+      hour: "numeric",
+      minute: "numeric",
+    };
+  
+    const optionsWeek = {
+      weekday: "long",
+      hour: "numeric",
+      minute: "numeric",
+    };
+  
+    const optionsPastWeek = {
+      weekday: "short",
+      month: "short",
+      day: "numeric",
+      hour: "numeric",
+      minute: "numeric",
+    };
+  
+    if (
+      adjustedMessageDate.getDate() === currentDate.getDate() &&
+      adjustedMessageDate.getMonth() === currentDate.getMonth() &&
+      adjustedMessageDate.getFullYear() === currentDate.getFullYear()
+    ) {
+      if (
+        index === 0 ||
+        adjustedMessageDate.getMinutes() !==
+          new Date(messages[index - 1].timestamp).getMinutes()
+      ) {
+        return adjustedMessageDate.toLocaleTimeString([], optionsToday);
+      }
+    } else if (
+      adjustedMessageDate.getTime() >=
+      currentDate.getTime() - 7 * 24 * 60 * 60 * 1000
+    ) {
+      if (
+        index === 0 ||
+        adjustedMessageDate.getMinutes() !==
+          new Date(messages[index - 1].timestamp).getMinutes()
+      ) {
+        return adjustedMessageDate.toLocaleTimeString([], optionsWeek);
+      }
+    } else {
+      return adjustedMessageDate.toLocaleString("en-US", optionsPastWeek);
+    }
+  };
+
+  const sendMessage = async () => {
+    const messageContent = messageInputRef.current.value;
+    if (messageContent) {
+      try {
+        await axios.post("http://localhost:8001/messages", {
+          sender_id: UsrID,
+          recipient_id: id,
+          message_content: messageContent,
+          timestamp: new Date().toISOString(),
+        });
+        messageInputRef.current.value = "";
+        await fetchMessages();
+        scrollToBottom();
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  };
+
+  const scrollToBottom = () => {
+    const container = messageContainerRef.current;
+    container.scrollTop = container.scrollHeight;
+  };
+
+  useEffect(() => {
+    fetchMessages();
+
+    ws.current = new WebSocket('ws://localhost:8001');
+
+    ws.current.onmessage = (event) => {
+      const newMessage = JSON.parse(event.data);
+      setMessages((prevMessages) => [...prevMessages, newMessage]);
+      scrollToBottom();
+    };
+
+    return () => {
+      if (ws.current && ws.current.readyState === WebSocket.OPEN) {
+        ws.current.close();
+      }
+    };
+  }, [id, UsrID]);
+
+  useEffect(() => {
+    scrollToBottom();
+  }, [messages]);
+
+  return (
+    <div className="AppScreen Messages">
+      <div className="holderOfNamesAndImg">
+        {info?.img !== null && info?.img !== "" ? (
+          <img
+            src={`http://localhost:8001/uploads/${info?.img}`}
+            className="userImage MEssagesApp"
+            alt="User"
+          />
+        ) : (
+          <div className="userImage noImg MEssagesApp" alt="User">
+            {(info?.name)?.slice(0, 1)}
+          </div>
+        )}
+        <span>
+          {info?.name} {info?.surname}
+        </span>
+      </div>
+      <div className="MessagesWrapper">
+        <div className="message-container" ref={messageContainerRef}>
+          {messages.map((message, index) => (
+            <div
+              key={message.message_id}
+              className={`message ${
+                message.sender_id === UsrID ? "Sender" : "Receiver"
+              }`}
+            >
+              <p>{formatTimestamp(message.timestamp, index)}</p>
+              <span>{message.message_content}</span>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="message-input">
+        <div className="inputHoldixonre">
+          <input type="text" ref={messageInputRef} placeholder="iMessage" />
+          <button onClick={sendMessage}>^</button>
+        </div>
+      </div>
+
+      <Draggable
+        axis="y"
+        position={{ x: 0, y: 0 }}
+        onStart={handleDragStart}
+        onDrag={handleDrag}
+      >
+        <div className="backBar calculator"></div>
+      </Draggable>
+    </div>
+  );
+}
---
+++ server.js
@@ -0,0 +1,339 @@
+const express = require('express');
+const axios = require('axios');
+const cors = require('cors');
+const mysql = require('mysql');
+const http = require('http');
+const bcrypt = require('bcrypt');
+const { v4: uuidv4 } = require('uuid');
+
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
+const path = require('path');
+const fs = require('fs');
+
+const PORT = process.env.PORT || 8001;
+const app = express();
+require('dotenv').config();
+
+const {Server} = require('ws');
+const server = http.createServer(app);
+const wsS = new Server({ server });
+
+
+app.use(cors());
+app.use(express.json())
+
+const db = mysql.createConnection({
+  host: `${process.env.HOSTLOCAL}`,
+  user: `${process.env.USERLOCAL}`,
+  // password: `${process.env.PASSWORDLOCAL}`,
+  database: `${process.env.DBNAMELOCAL}`,
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error('Error connecting to the database:', err);
+  } else {
+    console.log('Connected to the database');
+  }
+});
+
+db.on('error', (err) => {
+  console.error('MySQL connection error:', err);
+  if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+    db.connect((err) => {
+      if (err) {
+        console.error('Error reconnecting to the database:', err);
+      } else {
+        console.log('Reconnected to the database');
+      }
+    });
+  } else {
+    throw err;
+  }
+});
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
+app.get('/weather/:region', async (req, res) => {
+  try {
+    const weather = await axios.get(
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${req.params.region}/next7days?unitGroup=metric&key=${process.env.WEATHER_KEY}&contentType=json`
+    );
+    res.json(weather.data);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: 'Failed to fetch weather data' });
+  }
+});
+
+app.post('/signup', async (req, res) => {
+  const { email, password, name, surname } = req.body;
+  const id_uniq = uuidv4().substring(0, 20)
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const query = 'INSERT INTO users (id_uniq, email, password, name, surname) VALUES (?, ?, ?, ?, ?)';
+  db.query(query, [id_uniq, email, hashedPassword, name, surname], (err, result) => {
+    if (err) {
+      console.error('Error executing MySQL query:', err);
+      res.status(500).json({ error: 'Failed to create user' });
+    } else {
+      res.status(200).json({ message: 'User created successfully', id_uniq });
+    }
+  });
+});
+
+app.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+
+  const query = 'SELECT * FROM users WHERE email = ?';
+  db.query(query, [email], async (err, results) => {
+    if (err) {
+      console.error('Error executing MySQL query:', err);
+      res.status(500).json({ error: 'Failed to fetch user data' });
+    } else {
+      if (results.length === 0) {
+        res.status(401).json({ error: 'Invalid credentials' });
+      } else {
+        const user = results[0];
+        const passwordMatch = await bcrypt.compare(password, user.password);
+
+        if (passwordMatch) {
+      
+          res.status(200).json({
+            name: user.name,
+            surname: user.surname,
+            email: user.email,
+            id: user.id_user,
+            id_uniq: user.id_uniq,
+            img: user.img,
+          });
+        } else {
+          res.status(401).json({ error: 'Invalid credentials' });
+        }
+      }
+    }
+  });
+});
+
+app.put('/users/:id', async (req, res) => {
+  const { id } = req.params;
+  const { firstName, lastName } = req.body;
+
+  const query = 'UPDATE users SET name = ?, surname = ? WHERE id_user = ?';
+  db.query(query, [firstName, lastName, id], (err, result) => {
+    if (err) {
+      console.error('Error executing MySQL query:', err);
+      res.status(500).json({ error: 'Failed to update user' });
+    } else {
+      res.status(200).json({ message: 'User updated successfully' });
+    }
+  });
+});
+
+app.get('/user/:id', async (req, res) => {
+  const { id } = req.params;
+
+  const query = 'SELECT * FROM users WHERE id_user = ?';
+  db.query(query, [id], (err, result) => {
+    if (err) {
+      console.error('Error executing MySQL query:', err);
+      res.status(500).json({ error: 'Failed to fetch user data' });
+    } else {
+      res.status(200).json(result);
+    }
+  });
+})
+
+
+app.post('/upload/:id', upload.single('file'), (req, res) => {
+  const file = req.file;
+  const { id } = req.params;
+  if (!file) {
+    res.status(400).json({ error: 'No file provided' });
+    return;
+  }
+
+  const randomName = Math.random().toString(36).substring(7);
+  const fileExtension = path.extname(file.originalname);
+  const fileName = `${randomName}${fileExtension}`;
+  
+  const destinationPath = path.join(__dirname, 'uploads', fileName);
+
+  fs.rename(file.path, destinationPath, (err) => {
+    if (err) {
+      console.error('Error moving file:', err);
+      res.status(500).json({ error: 'Failed to move the file' });
+    } else {
+      const query = 'UPDATE users SET img = ? WHERE id_user = ?';
+      db.query(query, [fileName, id], (err, result) => {
+        if (err) {
+          console.error('Error executing MySQL query:', err);
+          res.status(500).json({ error: 'Failed to update the file name in the SQL table' });
+        } else {
+          res.status(200).json({ message: 'File uploaded and updated in the SQL table successfully', fileName});
+        }
+      });
+    }
+  });
+});
+
+app.get('/uploads/:filename', (req, res) => {
+  const { filename } = req.params;
+  const filePath = path.join(__dirname, 'uploads', filename);
+  res.sendFile(filePath);
+});
+
+
+
+app.post('/addfriend/:userId', async (req, res) => {
+  const { userId } = req.params;
+  const { friendId } = req.body;
+
+  const userQuery = 'SELECT * FROM users WHERE id_user = ?';
+  db.query(userQuery, [userId], (userErr, userResult) => {
+    if (userErr) {
+      console.error('Error executing MySQL query:', userErr);
+      res.status(500).json({ error: 'Failed to fetch user data' });
+    } else {
+      if (userResult.length === 0) {
+        res.status(404).json({ error: 'User not found' });
+        return;
+      }
+
+      const friendQuery = 'SELECT * FROM users WHERE id_user = ?';
+      db.query(friendQuery, [friendId], (friendErr, friendResult) => {
+        if (friendErr) {
+          console.error('Error executing MySQL query:', friendErr);
+          res.status(500).json({ error: 'Failed to fetch friend data' });
+        } else {
+          if (friendResult.length === 0) {
+            res.status(404).json({ error: 'Friend not found' });
+            return;
+          }
+
+          const friendshipQuery = 'SELECT * FROM userFriends WHERE (user_id = ? AND friend_id = ?) OR (user_id = ? AND friend_id = ?)';
+          db.query(friendshipQuery, [userId, friendId, friendId, userId], (friendshipErr, friendshipResult) => {
+            if (friendshipErr) {
+              console.error('Error executing MySQL query:', friendshipErr);
+              res.status(500).json({ error: 'Failed to check friendship' });
+            } else {
+              if (friendshipResult.length > 0) {
+                res.status(400).json({ error: 'Users are already friends' });
+                return;
+              }
+              const addFriendshipQuery = 'INSERT INTO userFriends (user_id, friend_id) VALUES (?, ?)';
+              db.query(addFriendshipQuery, [userId, friendId], (addErr, addResult) => {
+                if (addErr) {
+                  console.error('Error executing MySQL query:', addErr);
+                  res.status(500).json({ error: 'Failed to add friend' });
+                } else {
+                  res.status(200).json({ message: 'Friend added successfully' });
+                }
+              });
+            }
+          });
+        }
+      });
+    }
+  });
+});
+
+app.get('/friendlist/:userId', async (req, res) => {
+  const { userId } = req.params;
+
+  const userQuery = 'SELECT * FROM users WHERE id_user = ?';
+  db.query(userQuery, [userId], (userErr, userResult) => {
+    if (userErr) {
+      console.error('Error executing MySQL query:', userErr);
+      res.status(500).json({ error: 'Failed to fetch user data' });
+    } else {
+      if (userResult.length === 0) {
+        res.status(404).json({ error: 'User not found' });
+        return;
+      }
+
+      const friendListQuery = 'SELECT users.id_user, users.name FROM userFriends INNER JOIN users ON userFriends.friend_id = users.id_user WHERE userFriends.user_id = ?';
+      db.query(friendListQuery, [userId], (friendListErr, friendListResult) => {
+        if (friendListErr) {
+          console.error('Error executing MySQL query:', friendListErr);
+          res.status(500).json({ error: 'Failed to fetch friend list' });
+        } else {
+          const friendList = friendListResult.map((friend) => ({
+            id: friend.id_user,
+            name: friend.name,
+          }));
+
+          const reverseFriendListQuery = 'SELECT users.id_user, users.name FROM userFriends INNER JOIN users ON userFriends.user_id = users.id_user WHERE userFriends.friend_id = ?';
+          db.query(reverseFriendListQuery, [userId], (reverseFriendListErr, reverseFriendListResult) => {
+            if (reverseFriendListErr) {
+              console.error('Error executing MySQL query:', reverseFriendListErr);
+              res.status(500).json({ error: 'Failed to fetch friend list' });
+            } else {
+              const reverseFriendList = reverseFriendListResult.map((friend) => ({
+                id: friend.id_user,
+                name: friend.name,
+              }));
+
+              const mergedFriendList = [...friendList, ...reverseFriendList];
+
+              res.status(200).json({ friendList: mergedFriendList });
+            }
+          });
+        }
+      });
+    }
+  });
+});
+
+app.post('/messages', async (req, res) => {
+  const { sender_id, recipient_id, message_content, timestamp } = req.body;
+
+  const query = 'INSERT INTO messages (sender_id, recipient_id, message_content, timestamp) VALUES (?, ?, ?, ?)';
+  db.query(query, [sender_id, recipient_id, message_content, timestamp], (err, result) => {
+    if (err) {
+      console.error('Error executing MySQL query:', err);
+      res.status(500).json({ error: 'Failed to create message' });
+    } else {
+      res.status(200).json({ message: 'Message created successfully' });
+    }
+  });
+});
+
+app.get('/messages', async (req, res) => {
+  const { sender_id, recipient_id } = req.query;
+
+  const query = 'SELECT * FROM messages WHERE (sender_id = ? AND recipient_id = ?) OR (sender_id = ? AND recipient_id = ?)';
+  db.query(query, [sender_id, recipient_id, recipient_id, sender_id], (err, result) => {
+    if (err) {
+      console.error('Error executing MySQL query:', err);
+      res.status(500).json({ error: 'Failed to fetch messages' });
+    } else {
+      res.status(200).json(result);
+    }
+  });
+});
+
+
+  server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+wsS.on('connection', (socket, request) => {
+  console.log('WebSocket client connected');
+
+  socket.on('message', message => {
+    console.log('Received message:', message);
+    wsS.clients.forEach(client => {
+      console.log('Sending message:', message);
+      client.send(JSON.stringify(message));
+    });
+  });
+});
+
+
